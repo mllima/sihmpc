ae429774578938b73f2a575a1d2cdf8e2a8fc68e
Marcelo Lima <mllimabox-geral@yahoo.com.br>
2020-01-28 10:47:18 +0000

    WIP - testando possibilidades no cálculo do xN

--- sihmpc/ihmpccontroller.py
+++ sihmpc/ihmpccontroller.py
@@ -360,9 +360,9 @@ class IHMPCController(object):
         Y_pred = csd.vertcat(*Y_pred)
         U_pred = csd.vertcat(*U_pred)
         pred = csd.Function('pred', [X, U, csd.vertcat(*dU_pred)], 
-                            [X_pred, Y_pred, U_pred],
+                            [X_pred, Y_pred, U_pred, XN],
                             ['x0', 'u0', 'du_opt'],
-                            ['x_pred', 'y_pred', 'u_pred'])
+                            ['x_pred', 'y_pred', 'u_pred','xN'])
 
         prob = {'f': J, 'x': w, 'g': g, 'p': p} 
         bounds = {'lbw': lbw, 'ubw': ubw, 'lbg': lbg, 'ubg': ubg}
@@ -406,7 +406,13 @@ class IHMPCController(object):
         
         sol = solver(x0=W0, lbx=lbw, ubx=ubw, lbg=lbg, ubg=ubg, p=p,
                   lam_x0=LAM_W0, lam_g0=LAM_G0)
-        x_pred, y_pred, u_pred = pred(X,U,W0[:-2*self.ny])
+
+        import pdb
+        pdb.set_trace()
+
+        # x_pred, y_pred, u_pred, xN_pred = pred(X,U,W0[:-2*self.ny])
+        x_pred, y_pred, u_pred, xN_pred = pred(X,U,sol['x'][:-2*self.ny])
+      
 
         # loop para retornar o resultado em matriz
         du_opt = []
@@ -420,9 +426,9 @@ class IHMPCController(object):
         indx = (self.N-1)*self.nx
         MPC = csd.Function('MPC',
             [W0, X, Ysp, U, LAM_W0, LAM_G0, Pesos, ViN_ant],
-            [sol['f'], du_opt, sol['x'], sol['lam_x'], sol['lam_g'], sol['g'], sol['g'][indx:indx+self.nx], x_pred],
+            [sol['f'], du_opt, sol['x'], sol['lam_x'], sol['lam_g'], sol['g'], sol['g'][indx:indx+self.nx], x_pred, xN_pred],
             ['w0', 'x0', 'ySP', 'u0', 'lam_w0', 'lam_g0', 'pesos', 'ViN_ant'],
-            ['J','du_opt', 'w_opt', 'lam_w', 'lam_g', 'g', 'xN', 'x_pred'])
+            ['J','du_opt', 'w_opt', 'lam_w', 'lam_g', 'g', 'xN', 'x_pred', 'xN_pred'])
 
         return MPC
     
@@ -449,8 +455,13 @@ class IHMPCController(object):
         dustart = dustart.flatten()
         
         xii = sol['x_pred'][-self.nx:]
+        xiii = sol['xN_pred']
 
         xi = sol['xN']
+
+        import pdb
+        pdb.set_trace()
+
         ui = 0
         res = self.F(x0=xi, u0=ui, du0=0)
         Xknext = res['xkp1'].full()
